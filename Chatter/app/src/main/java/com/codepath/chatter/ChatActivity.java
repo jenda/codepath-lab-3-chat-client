b68a0a3894fe6c654a49e738e2d6f563c0c7b815
Jan Spidlen <jan.spidlen@airbnb.com>
2017-09-27 20:37:38 -0700

    Refreshing messages.

--- Chatter/app/src/main/java/com/codepath/chatter/ChatActivity.java
+++ Chatter/app/src/main/java/com/codepath/chatter/ChatActivity.java
@@ -71,7 +71,6 @@ public class ChatActivity extends AppCompatActivity {
         } else { // If not logged in, login as a new anonymous user
             login();
         }
-//        myHandler.postDelayed(mRefreshMessagesRunnable, POLL_INTERVAL);
 
         ParseLiveQueryClient parseLiveQueryClient = ParseLiveQueryClient.Factory.getClient();
 
@@ -114,6 +113,8 @@ public class ChatActivity extends AppCompatActivity {
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ChatActivity.this);
         linearLayoutManager.setReverseLayout(true);
         rvChat.setLayoutManager(linearLayoutManager);
+
+        refreshMessages();
     }
 
     void refreshMessages() {
